194e63daf3869a82014898e459842b5601ab1c26
Ben Clauser Ringia <54068142+benringia@users.noreply.github.com>
2020-05-18 00:11:56 +0800

    https

--- projects/moviefight/index.js
+++ projects/moviefight/index.js
@@ -14,7 +14,7 @@ const autoCompleteConfig = {
         return movie.Title;
     },
     async fetchData(searchTerm) {
-        const response = await axios.get('http://www.omdbapi.com/', {
+        const response = await axios.get('https://www.omdbapi.com/', {
             params: {
                 apikey: '3f9629b', //CHANGE THIS API KEY
                 s: searchTerm
