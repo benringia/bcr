d3b1b3d0d9752bcd52d2bbbb73967e85b4588eb3
Ben Clauser Ringia <54068142+benringia@users.noreply.github.com>
2020-05-31 15:55:10 +0800

    carousel and accordion

--- scripts/script.js
+++ scripts/script.js
@@ -56,6 +56,8 @@ siteHeader.classList.toggle('js-menu-opened');
 navigation.classList.toggle('js-show')
 });
 
+
+
 // Detect if user is using TAB to navigate
 function handleFirstTab(e) {
   if (e.keyCode === 9) {
@@ -117,3 +119,88 @@ $('.l-parallax-image').each(function() {
 	})
 });
 
+//CAROUSEL
+var slideIndex,slides,dots,captionText;
+function initGallery(){
+    slideIndex = 0;
+    slides=document.getElementsByClassName("imageHolder");
+    slides[slideIndex].style.opacity=1;
+
+    //disable nextPrevBtn if slide count is one
+    if(slides.length<2){
+        var nextPrevBtns=document.querySelector(".leftArrow,.rightArrow");
+        nextPrevBtns.style.display="none";
+        for (i = 0; i < nextPrevBtn.length; i++) {
+            nextPrevBtn[i].style.display="none";
+        }
+    }
+
+    //add dots
+    dots=[];
+    let dotsContainer=document.getElementById("dotsContainer"),i;
+    for (i = 0; i < slides.length; i++) {
+        var dot=document.createElement("span");
+        dot.classList.add("dots");
+        dotsContainer.append(dot);
+        dot.setAttribute("onclick","moveSlide("+i+")");
+        dots.push(dot);
+    }
+    dots[slideIndex].classList.add("active");
+}
+initGallery();
+function plusSlides(n) {
+    moveSlide(slideIndex+n);
+}
+function moveSlide(n){
+    let i;
+    let current,next;
+    const moveSlideAnimClass={
+          forCurrent:"",
+          forNext:""
+    };
+    let slideTextAnimClass;
+    if(n>slideIndex) {
+        if(n >= slides.length){n=0;}
+        moveSlideAnimClass.forCurrent="moveLeftCurrentSlide";
+        moveSlideAnimClass.forNext="moveLeftNextSlide";
+        slideTextAnimClass="slideTextFromTop";
+    }else if(n<slideIndex){
+        if(n<0){n=slides.length-1;}
+        moveSlideAnimClass.forCurrent="moveRightCurrentSlide";
+        moveSlideAnimClass.forNext="moveRightPrevSlide";
+        slideTextAnimClass="slideTextFromBottom";
+    }
+
+    if(n!=slideIndex){
+        next = slides[n];
+        current=slides[slideIndex];
+        for (i = 0; i < slides.length; i++) {
+            slides[i].className = "imageHolder";
+            slides[i].style.opacity=0;
+            dots[i].classList.remove("active");
+        }
+        current.classList.add(moveSlideAnimClass.forCurrent);
+        next.classList.add(moveSlideAnimClass.forNext);
+        dots[n].classList.add("active");
+        slideIndex=n;
+    }
+
+}
+
+// ACCORDION
+
+const initAccord = document.querySelectorAll('.accordion');
+for (let i = 0; i < initAccord.length; i++) {
+  initAccord[i].addEventListener("click", function() {
+    if (this.innerText=== "SHOW CERTIFICATES & LICENSES") {
+      this.innerText= "HIDE CERTIFICATES & LICENSES"
+    } else {
+      this.innerText= "SHOW CERTIFICATES & LICENSES"
+    }
+    
+    this.classList.toggle("active");
+    const child = this.nextElementSibling;
+    child.style.maxHeight ? child.style.maxHeight = null 
+    : child.style.maxHeight = child.scrollHeight + "px";
+  });
+}
